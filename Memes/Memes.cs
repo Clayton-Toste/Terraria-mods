9d6000849fd9371ac07d21a0b504cfb012b6b0cf
Nathan Pannell <nathan.pannell@berkeley.edu>
2017-12-01 17:57:04 -0800

    Added memes

---
+++ Memes/Memes.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+
+namespace Memes
+{
+	class Memes : Mod
+	{
+		public Memes()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
+	}
+}
