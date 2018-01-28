174b3b3a3eadaddf83577a6dba635b962084f459
Nathan Pannell <nathan.pannell@berkeley.edu>
2018-01-28 15:59:47 -0800

    started knuckles

--- Memes/Memes.cs
+++ Memes/Memes.cs
@@ -9,5 +9,13 @@ namespace Memes {
                 AutoloadSounds = true
             };
         }
+
+        // public Override void UpdateMusic(ref int music) {
+        //     if (Main.myPlayer != -1 && !Main.gameMenu) {
+        //         if (Main.player[Main.myPlayer].active && Main.player[Main.myPlayer].GetModPlayer<MyPlayer>(this).ZoneCustomBiome) {
+        //             // Yeah
+        //         }
+        //     }
+        // }
     }
 }
