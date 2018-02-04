9ec215028a9acaf8e10fa8913e4b3c2dcdbfd865
claytonmissile <claytontoste@gmail.com>
2018-02-04 15:11:32 -0800

    hope thats ok

--- Memes/Memes.cs
+++ Memes/Memes.cs
@@ -1,3 +1,4 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace Memes {
@@ -9,5 +10,13 @@ namespace Memes {
                 AutoloadSounds = true
             };
         }
+
+        public override void UpdateMusic(ref int music) {
+             if (Main.myPlayer != -1 && !Main.gameMenu) {
+                 if (Main.player[Main.myPlayer].active && (Main.LocalPlayer.FindBuffIndex(BuffType("DatBoiBuff")) != -1) {
+                    music = GetSoundSlot(SoundType.Music, "Sounds/DatBoiS");
+                }
+             }
+         }
     }
 }
