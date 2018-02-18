f761acf3335ec341b7aa3d8c7ac24cf1f87e7a8e
Nathan Pannell <nathan.pannell@berkeley.edu>
2018-02-18 15:54:35 -0800

    ayy

--- Memes/Memes.cs
+++ Memes/Memes.cs
@@ -13,14 +13,15 @@ namespace Memes {
 
         public override void UpdateMusic(ref int music) {
              if (Main.myPlayer != -1 && !Main.gameMenu) {
-                 if (Main.player[Main.myPlayer].active && (Main.LocalPlayer.FindBuffIndex(BuffType("DatBoiBuff")) != -1)) {
-                    music = GetSoundSlot(SoundType.Music, "Sounds/DatBoiS");
-                 }
-                 if (Main.player[Main.myPlayer].active && (Main.LocalPlayer.ZoneSandstorm)) {
-                    music = GetSoundSlot(SoundType.Music, "Sounds/Darude");
-                 }
+                 
+                if (Main.player[Main.myPlayer].active && (Main.LocalPlayer.ZoneSandstorm)) {
+                    music = GetSoundSlot(SoundType.Music, "Sounds/Music/Darude");
+                }
                 if (Main.player[Main.myPlayer].active && (Main.LocalPlayer.ZoneSnow)) {
-                    music = GetSoundSlot(SoundType.Music, "Sounds/Snowhalation");
+                    music = GetSoundSlot(SoundType.Music, "Sounds/Music/Snowhalation");
+                }
+                if (Main.player[Main.myPlayer].active && (Main.LocalPlayer.FindBuffIndex(BuffType("DatBoiBuff")) != -1)) {
+                    music = GetSoundSlot(SoundType.Music, "Sounds/DatBoiS");
                 }
             }
         }
--- Memes/NPCs/Boss/Knuckles.cs
+++ Memes/NPCs/Boss/Knuckles.cs
@@ -5,25 +5,28 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Chat;
 
 namespace Memes.NPCs.Boss {
     [AutoloadBossHead]
     public class Knuckles : ModNPC {
+
+        String state = "spinning";
+        int counter = 0;
+        Vector2 v;
 
         public override void SetStaticDefaults() {
             DisplayName.SetDefault("Ugandan Knuckles");
         }
 
         public override void SetDefaults() {
-            npc.aiStyle = 5;  //5 is the flying AI
+            npc.aiStyle = -1;  //5 is the flying AI
             npc.lifeMax = 145000 * 5;   //boss life
             npc.damage = 150;  //boss damage
             npc.defense = 80;    //boss defense
             npc.knockBackResist = 0f;
             npc.width = 788;
             npc.height = (1885 - 4) / 3;
-            animationType = NPCID.DemonEye;   //How the boss will behave
-            npc.aiStyle = NPCID.CrimsonAxe;
             Main.npcFrameCount[npc.type] = 3;    //boss frame/animation 
             npc.value = Item.buyPrice(0, 40, 75, 45);
             npc.npcSlots = 1f;
@@ -31,11 +34,57 @@ namespace Memes.NPCs.Boss {
             npc.lavaImmune = true;
             npc.noGravity = true;
             npc.noTileCollide = true;
+            UsesPartyHat();
             npc.HitSound = SoundID.NPCHit1;
             npc.DeathSound = SoundID.NPCDeath1;
             npc.buffImmune[24] = true;
-            music = MusicID.Boss2;
+            music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/Dounodawae");
             npc.netAlways = true;
+        }
+
+        public override void AI() {
+            npc.TargetClosest();
+            Player player = Main.player[npc.target];
+            if (state == "spinning") {
+                npc.velocity = Vector2.Zero;
+                npc.rotation += .5f;
+                if (npc.rotation > Math.PI) {
+                    npc.rotation = (float)-Math.PI;
+                }
+                counter += 1;
+                if (counter >= 1000 && Math.Round(npc.rotation) == Math.Round(npc.AngleTo(player.position))) {
+                    Vector2 moveTo = player.Center;
+                    Vector2 distance = moveTo - npc.Center;
+                    Vector2 v = distance * 0.02f;
+                    if (v.Length() > 10) {
+                        v *= 10 / v.Length();
+                    }
+                    npc.velocity = v;
+                    state = "charging";
+                }
+            }
+            if (state == "charging") {
+                counter -= 10;
+                if (counter <= 0) {
+                    Main.numStars = 130;
+                    Main.startSnowMoon();
+                    for (int i = 0; i < 130; i++)
+                    {
+                        Star.SpawnStars();
+                    }
+                    state = "spinning";
+                }
+            }
+            //Main.NewText(counter + "oooo");
+            //Main.NewText(Math.Round(npc.rotation) + "ff");
+            //Main.NewText(Math.Round(npc.AngleTo(player.position)));
+            //NetMessage.SendChatMessageToClient(NetworkText.FromLiteral("ayy"), Color.AliceBlue, player.whoAmI);
+            //npc.rotation = npc.AngleTo(player.position);
+        }
+
+        public override void OnHitPlayer(Player target, int damage, bool crit) {
+            base.OnHitPlayer(target, damage, crit);
+            target.AddBuff(BuffID.Bleeding, damage, false);
         }
 
         public override void BossLoot(ref string name, ref int potionType) {
